f5e76a48d72ced1188211d4c777b7761fe82777a
Luis Eufrasio <luis.eufrasio@gmail.com>
2016-01-25 01:42:08 +0000

    angular week 2 execise 3

--- web-dev/angular/conFusion/app/scripts/app.js
+++ web-dev/angular/conFusion/app/scripts/app.js
@@ -1,9 +1,10 @@
 'use strict';
 angular.module('confusionApp', [])
-    .controller('menuController', function() {
-        this.tab = 1;
-        this.filtText = '';
-        var dishes=[
+    .controller('MenuController', ['$scope', function($scope) {
+        $scope.tab = 1;
+        $scope.filtText = '';
+        $scope.showDetails = false;
+        $scope.dishes=[
             {
                 name:'Uthapizza',
                 image: 'images/uthapizza.png',
@@ -41,24 +42,26 @@ angular.module('confusionApp', [])
                 comment: ''
             }
         ]; 
-        this.dishes = dishes;
 
-        this.select = function(setTab) {
-            this.tab = setTab;
+        $scope.select = function(setTab) {
+            $scope.tab = setTab;
             if (setTab === 2) {
-                this.filtText = "appetizer";
+                $scope.filtText = "appetizer";
             }
             else if (setTab === 3) {
-                this.filtText = "mains";
+                $scope.filtText = "mains";
             }
             else if (setTab === 4) {
-                this.filtText = "dessert";
+                $scope.filtText = "dessert";
             }
             else {
-                this.filtText = "";
+                $scope.filtText = "";
             }
         };
-        this.isSelected = function (checkTab) {
-            return (this.tab === checkTab);
+        $scope.isSelected = function (checkTab) {
+            return ($scope.tab === checkTab);
         };
-    });
+        $scope.toggleDetails = function() {
+            $scope.showDetails = !$scope.showDetails;
+        };
+    }]);
