b39208df049d52202a4b1e81a5ae2936dbc748b3
Luis Eufrasio <luis.eufrasio@gmail.com>
2016-01-31 03:06:51 +0000

    angular week 3 exercise route-ui

--- web-dev/angular/conFusion/app/scripts/controllers.js
+++ web-dev/angular/conFusion/app/scripts/controllers.js
@@ -68,8 +68,8 @@ angular.module('confusionApp')
             };
         }])
 
-        .controller('DishDetailController', ['$scope', '$routeParams', 'menuFactory', function($scope, $routeParams, menuFactory) {
-            var dish= menuFactory.getDish(parseInt($routeParams.id, 10));
+        .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
+            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
             $scope.dish = dish;
         }])
 
